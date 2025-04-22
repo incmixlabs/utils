af36cda9ace371221fcc383a8c4a10ce98e95653
uma-uc <uma.apple123@gmail.com>
2025-04-22 13:34:47 -0700

    chore: add more tests

--- src/objects/omit.spec.ts
+++ src/objects/omit.spec.ts
@@ -6,3 +6,14 @@ describe("omit", () => {
     expect(omit(["a", "b"])({ a: "a", b: "b", c: "c" })).toEqual({ c: "c" })
   })
 })
+it("should handle omitting keys that don't exist", () => {
+  expect(omit(["d"])({ a: "a", b: "b", c: "c" })).toEqual({ a: "a", b: "b", c: "c" })
+})
+
+it("should handle empty keys array", () => {
+  expect(omit([])({ a: "a", b: "b" })).toEqual({ a: "a", b: "b" })
+})
+
+it("should handle empty object", () => {
+  expect(omit(["a", "b"])({})).toEqual({})
+})
