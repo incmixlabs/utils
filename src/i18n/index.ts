66a135da913e6c5607664f0fb0f6249dd434ac09
uma-uc <uma.apple123@gmail.com>
2025-04-26 22:58:38 -0700

    chore: intl-utils

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,9 +1,11 @@
 export { enTranslations } from "./en"
 export { ptTranslations } from "./pt"
 
-export type Language = "en" | "pt"
+export type Language = "en" | "pt" | "en-us" | "pt-BR"
 
 export const Locale: Record<Language, string> = {
-  en: "en",
+  en: "en-US",
+  "en-us": "en-US",
   pt: "pt-BR",
+  "pt-BR": "pt-BR"
 }
