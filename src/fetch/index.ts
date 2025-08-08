beabbd18adc8d9a29d98353a6ac5c0881a8f3a95
uma-uc <uma.apple123@gmail.com>
2025-08-07 22:42:02 -0700

    feat: utils and fetch

---
+++ src/fetch/index.ts
@@ -0,0 +1,14 @@
+export async function secureFetch<T>(
+  url: string,
+  init: RequestInit & { json?: unknown } = {}
+): Promise<T> {
+  const { json, ...opts } = init
+  const res = await fetch(url, {
+    credentials: "include",
+    headers: json ? { "content-type": "application/json" } : undefined,
+    body: json ? JSON.stringify(json) : undefined,
+    ...opts,
+  })
+  if (!res.ok) throw new Error(`${url} failed with ${res.status}`)
+  return res.json() as Promise<T>
+}
